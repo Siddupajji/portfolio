43b261b59f1a1715ca029cde1c605b4057ecb538
Siddupajji <76541945+Siddupajji@users.noreply.github.com>
2022-02-04 16:27:24 +0530

    edit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,44 @@
-import { NgModule } from '@angular/core';
+import { PhotoComponent } from './photo/photo.component';
+import { SmmComponent } from './smm/smm.component';
+import { GraphicComponent } from './graphic/graphic.component';
+import { HomeComponent } from './home/home.component';
+import { AppComponent } from './app.component';
+import { WebdevComponent } from './webdev/webdev.component';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ApplicationComponent } from './application/application.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', redirectTo: 'home', pathMatch: 'full'
+ },
+ {
+   path: 'home',
+   component: HomeComponent
+ },
+
+  {
+    path: 'web',
+    component: WebdevComponent
+  },
+  {
+    path: 'graphic',
+    component: GraphicComponent
+  },
+  {
+    path: 'application',
+    component: ApplicationComponent
+  },
+  {
+    path: 'smm',
+    component: SmmComponent
+  },
+  {
+    path: 'photo',
+    component: PhotoComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WebdevComponent } from './webdev/webdev.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { GraphicComponent } from './graphic/graphic.component';
+import { ApplicationComponent } from './application/application.component';
+import { SmmComponent } from './smm/smm.component';
+import { PhotoComponent } from './photo/photo.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WebdevComponent,
+    HomeComponent,
+    NavbarComponent,
+    GraphicComponent,
+    ApplicationComponent,
+    SmmComponent,
+    PhotoComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
